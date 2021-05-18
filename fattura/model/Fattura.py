d2ca9e7828a64c94eeb24b4c534f5fdcb28e569e
AntoColu <antonioby99@hotmail.it>
2021-05-18 22:00:20 +0200

    Creazione classe Fattura.py

---
+++ fattura/model/Fattura.py
@@ -0,0 +1,15 @@
+from fornitore.model.Fornitore import Fornitore
+from listaarticoli.model.ListaArticoli import ListaArticoli
+
+class Fattura():
+    def __init__(self, num_fatt, tipo, data, Cliente, Fornitore, ListaArticoli, totale):
+        super(Fattura, self).__init__()
+
+        self.num_fatt = num_fatt
+        self.tipo = tipo
+        self.data = data
+        #self.Cliente = Cliente
+        self.Fornitore = Fornitore
+        self.ListaArticoli = ListaArticoli
+        self.totale = totale
+
