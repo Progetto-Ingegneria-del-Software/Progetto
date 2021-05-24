ca8a118b0ca2939f08f59b4cfed1c0823fc452dd
FRANCESCO-Fer <73470700+FRANCESCO-Fer@users.noreply.github.com>
2021-05-24 12:41:19 +0200

    update vista home

--- home/views/VistaHome.py
+++ home/views/VistaHome.py
@@ -1,3 +1,4 @@
+
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, \
     QHBoxLayout, QComboBox, QLabel, QSizePolicy, QFrame, QHeaderView
 
@@ -6,6 +7,68 @@ from listadipendenti.view.VistaListaDipendenti import VistaListaDipendenti
 from listafornitori.view.VistaListaFornitori import VistaListaFornitori
 from home.views.logoo import App
 
+
+
+
+
+
+'''
+class VistaHome(QWidget):
+    def __init__(self, parent=None):
+        super(VistaHome, self).__init__(parent)
+
+
+            tabwidget = QTabWidget()
+            tabwidget.addTab(label1, "Tab 1")
+            tabwidget.addTab(label2, "Tab 2")
+            layout.addWidget(tabwidget, 0, 0)
+
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 tabs - pythonspot.com'
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 200
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+
+        self.show()
+
+
+class VistaHome(QWidget):
+
+    def __init__(self):
+        super(QWidget, self).__init__()
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QFrame()
+        self.tab1.addwidget
+        #self.tab2 = QWidget()
+        self.tabs.resize(1600, 500)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Tab 1")
+       # self.tabs.addTab(self.tab2, "Tab 2")
+
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.vista_lista_dipendenti = VistaListaDipendenti()
+        self.vista_lista_dipendenti.show()
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Add tabs to widget
+       # self.layout.addWidget(self.tabs)
+       # self.setLayout(self.layout)
+'''
 class VistaHome(QWidget):
         def __init__(self, parent=None):
             super(VistaHome, self).__init__(parent)
@@ -22,6 +85,8 @@ class VistaHome(QWidget):
             tabs.addTab(self.Dipendenti(), "Dipendenti")
             tabs.addTab(self.Articoli(), "Articoli")
             tabs.addTab(self.Fornitori(), "Forinitori")
+            tabs.addTab(self.Clienti(), "Clienti")
+            tabs.addTab(self.ClientiPIva(), "Clienti PIVA")
 
             layout.addWidget(tabs)
 
@@ -68,6 +133,34 @@ class VistaHome(QWidget):
 
             generalTab.setLayout(layout)
             return generalTab
+        
+        
+        
+        
+        def ClientiPIva(self):
+            """Create the General page UI."""
+            generalTab = QWidget()
+            layout = QVBoxLayout()
+
+            layout.addWidget(Vista_lista_clientipiva())
+
+            generalTab.setLayout(layout)
+
+            return generalTab 
+        
+        
+        def Clienti(self):
+            """Create the General page UI."""
+            generalTab = QWidget()
+            layout = QVBoxLayout()
+
+
+
+            layout.addWidget(Vista_Lista_clienti())
+
+            generalTab.setLayout(layout)
+            return generalTab
+
 
 '''
 class Window(QWidget):
